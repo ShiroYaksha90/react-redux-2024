07ad22947661483ee62a1b37e3231e2822e40ecd
ShiroYaksh90 <aljazaeribasim@gmail.com>
2025-09-14 14:05:31 +0300

    refactor: update WatchedBox and WatchedMovieList components.

--- showey/src/App.jsx
+++ showey/src/App.jsx
@@ -138,37 +138,37 @@ const WatchedBox = () => {
       {isOpen2 && (
         <>
           <Summary watched={watched} />
-          <ul className="list">
-            {watched.map((movie) => (
-              <WatchedMovieList movie={movie} key={movie.imdbID} />
-            ))}
-          </ul>
+          <WatchedMovieList movies={watched} key={watched.imdbID} />
         </>
       )}
     </div>
   );
 };
 
-const WatchedMovieList = ({ movie }) => {
+const WatchedMovieList = ({ movies }) => {
   return (
-    <li>
-      <img src={movie.Poster} alt={`${movie.Title} poster`} />
-      <h3>{movie.Title}</h3>
-      <div>
-        <p>
-          <span>⭐️</span>
-          <span>{movie.imdbRating}</span>
-        </p>
-        <p>
-          <span>🌟</span>
-          <span>{movie.userRating}</span>
-        </p>
-        <p>
-          <span>⏳</span>
-          <span>{movie.runtime} min</span>
-        </p>
-      </div>
-    </li>
+    <ul className="list">
+      {movies?.map((movie) => (
+        <li>
+          <img src={movie.Poster} alt={`${movie.Title} poster`} />
+          <h3>{movie.Title}</h3>
+          <div>
+            <p>
+              <span>⭐️</span>
+              <span>{movie.imdbRating}</span>
+            </p>
+            <p>
+              <span>🌟</span>
+              <span>{movie.userRating}</span>
+            </p>
+            <p>
+              <span>⏳</span>
+              <span>{movie.runtime} min</span>
+            </p>
+          </div>
+        </li>
+      ))}
+    </ul>
   );
 };
 
