9045f0ad90a891a20c779615a8b28e4a019a613b
ShiroYaksh90 <aljazaeribasim@gmail.com>
2025-10-04 06:49:39 +0300

    refactor: enhance StarRating component with customizable color and size props; improve message display logic

--- showey/src/StarRating.jsx
+++ showey/src/StarRating.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+// import propTypes from ""
 
 const containerStyle = {
   display: "flex",
@@ -10,12 +11,12 @@ const starContainerStyle = {
   display: "flex",
 };
 
-const textStyle = {
-  lineHeight: "1.2",
-  margin: "0",
-};
-
-export const StarRating = ({ maxRating = 5 }) => {
+export const StarRating = ({
+  maxRating = 5,
+  color = "#fcc419",
+  size = "24px",
+  messages = [],
+}) => {
   const [rating, setRating] = useState(0);
   const [tempRating, setTempRating] = useState(0);
   const handleRating = (rating) => {
@@ -27,6 +28,13 @@ export const StarRating = ({ maxRating = 5 }) => {
   const onHoverOut = () => {
     setTempRating(0);
   };
+
+  const textStyle = {
+    lineHeight: "1.2",
+    margin: "0",
+    color,
+    fontSize: size,
+  };
   return (
     <div style={containerStyle}>
       <div style={starContainerStyle}>
@@ -37,22 +45,27 @@ export const StarRating = ({ maxRating = 5 }) => {
             onMouseEnter={() => onHoverIn(i + 1)}
             onMouseLeave={() => onHoverOut(i + 1)}
             full={rating >= i + 1 || tempRating >= i + 1}
+            color={color}
+            size={size}
           />
         ))}
       </div>
-      <div style={textStyle}>{tempRating || rating || ""}</div>
+      <div style={textStyle}>
+        {messages.length === maxRating
+          ? messages[tempRating - 1] || messages[rating - 1] || ""
+          : tempRating || rating || ""}
+      </div>
     </div>
   );
 };
 
-const starStyle = {
-  width: "24px",
-  height: "24px",
-  display: "block",
-  cursor: "pointer",
-};
-
-const Star = ({ onRate, full, onMouseEnter, onMouseLeave }) => {
+const Star = ({ onRate, full, onMouseEnter, onMouseLeave, color, size }) => {
+  const starStyle = {
+    width: size,
+    height: size,
+    display: "block",
+    cursor: "pointer",
+  };
   return (
     <span
       role="button"
@@ -65,8 +78,8 @@ const Star = ({ onRate, full, onMouseEnter, onMouseLeave }) => {
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 20 20"
-          fill="#000"
-          stroke="#000"
+          fill={color}
+          stroke={color}
         >
           <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
         </svg>
@@ -75,7 +88,7 @@ const Star = ({ onRate, full, onMouseEnter, onMouseLeave }) => {
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
-          stroke="#000"
+          stroke={color}
         >
           <path
             strokeLinecap="round"
