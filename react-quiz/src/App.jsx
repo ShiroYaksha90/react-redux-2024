27e092b7bfe71e3ce68b13500df100fa864fdf62
ShiroYaksh90 <aljazaeribasim@gmail.com>
2025-12-15 22:54:40 +0300

    Add useReducer hook to fetch data from fakeApi

--- react-quiz/src/App.jsx
+++ react-quiz/src/App.jsx
@@ -1,13 +1,40 @@
+import { useReducer, useEffect } from "react";
 import Header from "./Header";
 import MainComponent from "./MainComponent";
+import Loader from "./Loader";
+import Error from "./Error";
+import StartScreen from "./StartScreen";
+const initialState = { questions: [], status: "loading", error: "" };
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReady":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataError":
+      return { ...state, error: action.payload, status: "error" };
+    default:
+      throw new Error("Unknown action");
+  }
+}
 export default function App() {
+  const [{ questions, status, error }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+  const numQuestions = questions.length;
+  useEffect(function () {
+    fetch("http://localhost:4000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReady", payload: data }))
+      .catch((err) => dispatch({ type: "dataError", payload: err.message }));
+  }, []);
   return (
     <div className="App">
       <div className="app">
         <Header />
         <MainComponent>
-          <p>1/15</p>
-          <p>Question?</p>
+          {status === "loading" && <Loader />}
+          {status === "error" && <Error error={error} />}
+          {status === "ready" && <StartScreen numQuestions={numQuestions} />}
         </MainComponent>
       </div>
     </div>
